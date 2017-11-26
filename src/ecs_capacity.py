dce8fd8cebf293bca0c995965ca4a50f46bfe95c
Justin Menga <justin.menga@gmail.com>
2017-11-26 14:36:12 +1300

    Add boto3 clients and configuration variables

--- src/ecs_capacity.py
+++ src/ecs_capacity.py
@@ -13,3 +13,13 @@ log = logging.getLogger()
 log.setLevel(os.environ.get('LOG_LEVEL','INFO'))
 def format_json(data):
   return json.dumps(data, default=lambda d: d.isoformat() if isinstance(d, datetime.datetime) else str(d))
+
+# AWS clients
+ecs = boto3.client('ecs')
+cloudwatch = boto3.client('cloudwatch')
+
+# Environment variables
+CONTAINER_MAX_MEMORY = int(os.environ.get('CONTAINER_MAX_MEMORY',993))
+CONTAINER_MAX_CPU = int(os.environ.get('CONTAINER_MAX_CPU',1024))
+TCP_PORT_RESOURCES = [p for p in os.environ.get('TCP_PORT_RESOURCES','').split(',') if p]
+UDP_PORT_RESOURCES = [p for p in os.environ.get('UDP_PORT_RESOURCES','').split(',') if p]
